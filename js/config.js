4e1b2bb4160fdc16c2e4e7c5e665531cda734901
Harry Ghazni <harryghazni@Harrys-iMac.local>
2018-04-09 16:26:23 +0100

    adds requirejs and fixes mobile dropdown nav issue with overlay

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// Configuration File
+
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: [
+            'vendor/jquery-min',
+            'https://code.jquery.com/jquery-3.3.1.min.js' 
+    ],
+        header: 'header',
+        easteregg: 'easteregg',
+        fontawesome: "https://use.fontawesome.com/releases/v5.0.9/js/all"
+    },
+});
