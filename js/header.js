a68b5383482662ae497ad63cbeb57922a727c669
Harry Ghazni <harryghazni@Harrys-iMac.local>
2018-04-16 15:59:56 +0100

    refactors markup and css further

--- js/header.js
+++ js/header.js
@@ -6,32 +6,32 @@ Desktop Dropdown Menu
 ================================*/
 $(document).ready(function() {
     // Hides menu(s) on document load
-    $(".dropdown-nav__wrapper").hide();
+    $(".dropnav__wrapper").hide();
     $(".mobile-menu__wrapper").hide();
         // Hides Mobile Overlay background
         $(".mobile-menu__overlay").hide();
     // Hides Products Menu
-    $(".mobile-menu__products--open").hide();
+    $(".mobilemenu__products--open").hide();
         
     // Toggles the dropdown menu
     $(".menu-item:nth-of-type(2)").mouseenter(function(){
-        $('.dropdown-nav__wrapper').delay(70).stop( true ).fadeToggle().toggleClass('active');
+        $('.dropnav__wrapper').delay(70).stop( true ).fadeToggle().toggleClass('active');
     });
 
     // If mouse leaves dropdown nav wrapper it hides it
-    $(".dropdown-nav__wrapper").mouseleave(function(){
-        $(".dropdown-nav__wrapper").delay(70).stop( true ).fadeOut().toggleClass('active');
+    $(".dropnav__wrapper").mouseleave(function(){
+        $(".dropnav__wrapper").delay(70).stop( true ).fadeOut().toggleClass('active');
     })
 
     // If mouse enters the header hide the dropdown menu
     $('.header__centrebar').mouseenter(function(){
-        $('.dropdown-nav__wrapper').delay(70).stop( true ).fadeOut().toggleClass('active');
+        $('.dropnav__wrapper').delay(70).stop( true ).fadeOut().toggleClass('active');
     });
 
     /* Prevent Right Click from hiding the dropdown. Mouse leave is being triggered due to a Chrome bug. Right click is being disabled on the dropdown menu for now (not a great solution). 
     
     At http://selcobw.com you can see the same bug cancelling the dropdown menu there too but it's slightly more delayed.*/ 
-    $( ".dropdown-nav__wrapper" ).on("contextmenu",function(){
+    $( ".dropnav__wrapper" ).on("contextmenu",function(){
         return false;
      }); 
 });
@@ -64,7 +64,7 @@ $(".mobile-menu__overlay").click(function(){
 
 // Products Button
 $(".mob-products").click(function() {
-    $(".mobile-menu__products--open").toggle();
+    $(".mobilemenu__products--open").toggle();
     $(".col.caret").toggleClass("active");
     $(".col.caret a").toggleClass("active");
 })
